d4cfa4b87e968770b236accaa899e2ca8e3ed4d1
JhonDCT <deyner.catacora@gmail.com>
2024-12-18 19:35:59 -0500

    add google auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Auth,
+  AuthModule,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  User,
+} from '@angular/fire/auth';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AuthModule, AsyncPipe, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'shop';
+  private userSubject: BehaviorSubject<User | null> =
+    new BehaviorSubject<User | null>(null);
+  public user$: Observable<User | null> = this.userSubject.asObservable();
+
+  constructor(private auth: Auth) {
+    onAuthStateChanged(auth, (user) => {
+      this.userSubject.next(user);
+    });
+  }
+
+  async loginWithGoogle(): Promise<void> {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(this.auth, new GoogleAuthProvider());
+  }
+
+  async logout(): Promise<void> {
+    await this.auth.signOut();
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,29 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'abancay-shop-pos',
+        appId: '1:415867153722:web:439241a0c037043e0ff403',
+        storageBucket: 'abancay-shop-pos.firebasestorage.app',
+        // locationId: 'us-central',
+        apiKey: 'AIzaSyCblIE6uLJiHO0B4Eb82G9JrSCUu7bS8-Y',
+        authDomain: 'abancay-shop-pos.firebaseapp.com',
+        messagingSenderId: '415867153722',
+        measurementId: 'G-485XKZZTE2',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideMessaging(() => getMessaging()),
+  ],
 };
